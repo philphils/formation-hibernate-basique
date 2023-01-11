d6c6946cffa11ba4a5b7978102c3eb2008de1759
Philippe SABAA <philippe.sabaa@insee.fr>
2023-01-11 15:27:42 +0100

    Ajout @TestInstance pour méthode non static

--- formation-hibernate/src/test/java/fr/insee/formation/hibernate/config/AbstractTest.java
+++ formation-hibernate/src/test/java/fr/insee/formation/hibernate/config/AbstractTest.java
@@ -1,5 +1,7 @@
 package fr.insee.formation.hibernate.config;
 
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 @ActiveProfiles("test")
+@TestInstance(Lifecycle.PER_CLASS)
 public abstract class AbstractTest {
 
 }
