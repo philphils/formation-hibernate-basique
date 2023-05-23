efdf367d87530702f0bebaa1c003435e31ba9e91
Philippe SABAA <philippe.sabaa@insee.fr>
2023-05-23 11:40:02 +0200

    Ajout commentaire aide conversion Date Year YearMonth

--- formation-hibernate/src/main/java/fr/insee/formation/hibernate/model/IndiceAnnuel.java
+++ formation-hibernate/src/main/java/fr/insee/formation/hibernate/model/IndiceAnnuel.java
@@ -4,17 +4,19 @@ import java.time.Year;
 
 public class IndiceAnnuel extends Indice {
 
-    /**
-     * On peut utiliser un Converter vers le format Date
-     */
-    private Year year;
+	/**
+	 * On peut utiliser un Converter vers le format Date Petite aide, code pour
+	 * obtenir le Year à partir d'une Date :
+	 * Year.from(declaration.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+	 */
+	private Year year;
 
-    public Year getYear() {
-        return year;
-    }
+	public Year getYear() {
+		return year;
+	}
 
-    public void setYear(Year year) {
-        this.year = year;
-    }
+	public void setYear(Year year) {
+		this.year = year;
+	}
 
 }
--- formation-hibernate/src/main/java/fr/insee/formation/hibernate/model/IndiceMensuel.java
+++ formation-hibernate/src/main/java/fr/insee/formation/hibernate/model/IndiceMensuel.java
@@ -4,17 +4,19 @@ import java.time.YearMonth;
 
 public class IndiceMensuel extends Indice {
 
-    /**
-     * On peut utiliser un Converter vers le format Date
-     */
-    private YearMonth month;
+	/**
+	 * On peut utiliser un Converter vers le format Date Petite aide, code pour
+	 * obtenir le YearMonth à partir d'une Date :
+	 * YearMonth.from(declaration.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+	 */
+	private YearMonth month;
 
-    public YearMonth getMonth() {
-        return month;
-    }
+	public YearMonth getMonth() {
+		return month;
+	}
 
-    public void setMonth(YearMonth month) {
-        this.month = month;
-    }
+	public void setMonth(YearMonth month) {
+		this.month = month;
+	}
 
 }
